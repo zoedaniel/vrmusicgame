442340a5e1cdfa270169cff9e6449c4ea243ece2
zoedaniel <zoe.olivia.daniel@gmail.com>
2025-04-30 19:12:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 // Algorithm for detecting frequency mistakes:
-// Gather recordings: golden truth recording, student recording
-// Identify notes for each recording (convert recording to list of notes)
-// Golden truth: a, b, c, b, a
-//           0  1  2  3  4
-// golden_truth[0] = a
-// golden_truth[1] = b
-// golden_truth[2] = c
-// golden_truth[3] = b
-// golden_truth[4] = a
-// Student: a, b, c, a flat, a
-// Compare the lists and identity/record differences 
-// Tell user what differences/mistakes were
+// 1. Gather recordings: golden truth recording, student recording
+// TODO(zoe): get recording from mp3
+
+// 2. Identify notes for each recording (convert recording to list of notes)
+// TODO(zoe): hardcoding notes for now, utilize audio library later (mp3-->frequencies-->notes)
+var goldenNotes = ['A', 'D', 'C', 'B', 'A'];
+var perfectStudentNotes = ['A', 'D', 'C', 'B', 'A'];
+var incorrectStudentNotes = ['E', 'D', 'B', 'B', 'A'];
+var tooManyStudentNotes = ['A', 'D', 'C', 'B', 'A', 'G'];
+
+// 3. Compare the lists and identity/record differences 
+function compareNotes(goldenNotes, studentNotes) {
+  for (let i = 0; i < goldenNotes.length; i++) {
+    console.log(i);
+  }
+}
+
+// 4. Tell user what differences/mistakes were
+compareNotes(goldenNotes, perfectStudentNotes);
+
 // Try again!
+
